562d03ce6f65d9bae6ae0a76dc0d5dd3241f0052
pjungjs <pjungjs@gmail.com>
2023-02-09 18:39:41 -0500

    passed all the tests plus bonus.

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,7 @@ function Form() {
   const [textInput, setTextInput] = useState("");
   const [selectOption, setSelectOption] = useState("");
   const [result, setResult] = useState("");
+  const [error, setError] = useState(false);
 
   function handleTextChange(event) {
     //user's text input value.
@@ -16,65 +17,82 @@ function Form() {
     setSelectOption(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    calculate();
+  }
+
+  function handleError() {
+    setError(true);
+    setResult("Invalid input.");
+  }
+
   function resetForm() {
     setTextInput("");
     setSelectOption("");
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    calculate();
-    resetForm();
-  }
-
   function calculate() {
     let total = 0;
     const objMode = {};
-    const arr = textInput.split(",");
-
-    if(selectOption === "" || textInput === "") {
-      setResult("Invalid input.");
-    } else {
+    const arrOfNum = textInput.split(",");
+    console.log(textInput)
+    
+    if (selectOption) {
       //sum
-      arr.forEach(num => total += Number(num));
-      console.log(total)
+      arrOfNum.forEach(num => total += Number(num));
       
       //average & mode
       if(selectOption === "average") {
         //get the value from sum, then divide it by the length of the array.
-        total /= arr.length;
+        total /= arrOfNum.length;
 
       } else if(selectOption === "mode") {
-        // //the first index is the number repeated,
-        // //the second index is the repetition number.
-        // let repeatedNum = 0;
-        // let repeatedTimes = 1;
-
-        arr.forEach(num => {
+        arrOfNum.forEach(num => {
           if(objMode[num]) {
             objMode[num] += 1;
           } else {
             objMode[num] = 1;
           }
-
-          // //compare it and assign the number and its repetition number to the variables.
-          // if(objMode[num] > repeatedNum) {
-          //   repeatedNum = num;
-          //   repeatedTimes = objMode[num];
-          // }
         });
 
+        //find the highest value
         total = Object.keys(objMode).reduce((a, b) => objMode[a] > objMode[b] ? a : b);
+
+        // //compare it and assign the number and its repetition number to the variables.
+        // let repeatedNum = 0;
+        // let repeatedTimes = 1;
+        // for(let num in objMode) {  
+        //   if(objMode[num] > repeatedNum) {
+        //     repeatedNum = num;
+        //     repeatedTimes = objMode[num];
+        //   }
+        // }
+        // total = repeatedNum
       }
       setResult(total);
+    }
+
+    //conditional to handle errors, or hide the class and reset the form.
+    if(!selectOption || !textInput || isNaN(total)) {
+      handleError();
+    } else {
+      setError(false);
+      resetForm();
     }
   }
 
   return (
     <>
       <form onSubmit={handleSubmit} >
-        <input id="values" name="values" type="text" value={textInput} onChange={handleTextChange} />
-        <select id="operation" name="operation" onChange={handleSelectChange}>
+        <input id="values" name="values" type="text"
+          value={textInput} onChange={handleTextChange}
+          className={error ? "error" : ""}
+        />
+        <select id="operation" name="operation"
+          value={selectOption} onChange={handleSelectChange}
+          className={error ? "error" : ""}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
