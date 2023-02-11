26a2723f53f048a854a59395e39c70f720a60a50
Angel <EMAIL>
2023-02-11 12:49:31 -0500

    forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
+  const [result, setResult] = useState('');
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
-      <section id="result">
-        <p></p>
-      </section>
+      <Form setResult={setResult}/>
+      <section id='result'>
+        <p>{result}</p>
+        </section>
     </main>
   );
 }
