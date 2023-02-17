25f2e28af3df8b4d1cc305220089ca9e87e4f4fc
JohnnyMCR <jmcastillo991@gmail.com>
2023-02-16 20:02:50 -0500

    oldCode

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,47 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
+
+
 
 function Form() {
+
+  const [textEntry, updateText] = useState("");
+  const [selectOption, updateSelectOption] = useState("");
+
+  function updateTextEntry(event) {
+    updateText(event.target.value)
+  }
+
+  function updateSelectOptionEntry(event) {
+    updateSelectOption(event.target.value)
+  }
+
+  function numberTest(currentValue) {
+    return !isNaN(Number(currentValue));
+  }
+  function onSubmitCalculate(event) {
+    event.preventDefault();
+    const arrayInputs
+    const
+
+  }
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
-        <option value=""></option>
-        <option value="sum">sum</option>
-        <option value="average">average</option>
-        <option value="mode">mode</option>
-      </select>
-      <button type="submit">Calculate</button>
-    </form>
+    <>
+      <form>
+        <input onChange={updateTextEntry}id="values" name="values" type="text" />
+        <select onChange={updateSelectOptionEntry} id="operation" name="operation">
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button onSubmit={onSubmitCalculate} type="submit">Calculate</button>
+      </form>
+      <section id="result">
+        <p></p>
+      </section>
+    </>
   );
 }
 
