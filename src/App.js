379a5c3572cacbd94a0b109df9b7054b9960d4c4
RadiantRay22 <rayrussell@pursuit.org>
2023-02-19 08:08:41 -0500

    Ray is Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
+
+
+
+
 import React from "react";
-import Form from "./Form";
+import TopBar from "./Components/TopBar";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 import "./App.css";
+
+const targetAmount = 1000;
+const donations = [
+  {
+    amount: 250,
+    caption: "You really need this. Really.",
+    id: 1,
+    name: "Jo",
+  },
+  {
+    amount: 30,
+    caption: "Here, take a break from work!",
+    id: 2,
+    name: "Rami",
+  },
+  {
+    amount: 20,
+    caption: "LOL! You are too funny. Happy to do this for you. :)",
+    id: 3,
+    name: "Michelle",
+  },
+  {
+    amount: 5,
+    caption: "Have fun!",
+    id: 4,
+    name: "Malinda",
+  },
+  {
+    amount: 30,
+    caption: "Come visit me in Miami!",
+    id: 5,
+    name: "Sam",
+  },
+];
+const initialValue = 0;
+const currentTotal = donations.reduce((accumulator, currentDonation) =>
+accumulator + currentDonation.amount, 0);
 
 function App() {
   return (
-    <main>
-      <p>Enter each number in the array, separated by a ','</p>
-      <Form />
-      <section id="result">
-        <p></p>
-      </section>
-    </main>
+    <>
+      <TopBar />
+      <main className="container">
+        <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar"><RecentDonations donations = {donations}/></section>
+        <section className="">
+          {/* Progress */}
+          {/* Donation Form */}
+          <Progress currentTotal = {currentTotal} targetAmount = {targetAmount}/>
+          <DonationForm donations = {donations}/>
+        </section>
+      </main>
+    </>
   );
 }
-
 export default App;
