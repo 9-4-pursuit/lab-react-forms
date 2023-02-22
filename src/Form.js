552a011a368492b9827bbe73d951754d7fcce462
MichelBDLC <michelbdlc@gmail.com>
2023-02-22 00:32:40 -0800

    we're getting there

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+
+  const [selectOption, setSelectOption] = useState("");
+  const [num, setNumbers] = useState(0);
+  const [input, setInput] = useState({
+    num: "",
+    calculate: "",
+  });
+
+  function handleSelectChange(event) {
+    setSelectOption(event.target.value);
+  }
+
+  function handleNumberInputChange(event) {
+    setNumbers(event.target.value);
+  }
+
+  function handleCalculate(event) {
+    event.preventDefault();
+
+    // if (input.num.value < 1 || input.calculate.value < 1) {
+
+    //     let p = document.createElement('p');
+    //     document.querySelector('form').appendChild(p);
+    //     p.textContent = "invalid input."
+   
+    // }
+
+    resetForm();
+  }
+
+  function handleInput(event) {
+    setInput({
+      ...input, [event.target.id]: event.target.value
+    });
+  }
+
+  function resetForm() {
+    setInput({
+      num: "",
+      calculate: "",
+    });
+  }
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleCalculate}>
+      <input id="values" name="values" type="text" onChange={handleNumberInputChange}/>
+      <select id="operation" name="operation" onChange={handleSelectChange}>
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
