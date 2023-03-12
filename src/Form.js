53c7f9570536ec5cb93cb09f640ce92a47fdb217
JenniferEinstein <jennifereinstein@gmail.com>
2023-03-12 11:39:43 -0400

    changes

--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,41 @@
 
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
 
-  // const [input, setInput] = useState("")
-  // const [result, setResult] = useState(null)
+  // const [operation, setOperation] = useState("")
+  // const [value, setValue] = useState("")
+  const [input, setInput] = useState("")
+  const [result, setResult] = useState("")
+  const [selectOption, setSelectOption] = useState("");
 
+  // event.preventDefault()
+  // const [error, setError] = useState(false)
+  // function invalidInput (numbers) {
+  //   const allowable = "all"
+
+  /*Guard Clause  
+    if any letters are in the input, return "Invalid input."
+    if no operation is selected, return "Invalid input."
+*/
+  
   // function handleInputFunction(event) = 
   // event.target.value
 
-  function showResult(){
 
+  function handleSelectChange(event){
+    setSelectOption(event.target.value)
+    console.log(selectOption)
+  }
+
+
+
+  function handleSubmitEvent(event){
+    event.preventDefault();
+    console.log("event is ", event)
+    console.log("Form Submitted!")
     let operation = event.target.value
     if (operation === "sum") {
       sumNumbers()
@@ -21,64 +45,64 @@ function Form() {
       modeNumbers()
     }
 
-  }
 
-  function sumNumbers(numbers){
-    //split the inputs into individual numbers and sum them
-    const newNumbers = Number(numbers.split(","));
-    let sum = 0
-    for (let i=0; i < newNumbers.length; i++) {
-      sum += newNumbers[i];
-    }
-    console.log(sum)
-    return sum
-  }
 
-      //number that appears most often
-  function modeNumbers(numbers){
-    const newNumber = Number(numbers.split(","));
-    const mode={}
-    let max = 0;
-    let count = 0;
-    for(let i=0; i < newNumber.length; i++){
-      const item = newNumber[i];
-      if(mode[item]) {
-        mode[item]++;
-      }else {
-        mode[item] = 1;
+    function sumNumbers(numbers){
+      //split the inputs into individual numbers and sum them
+      const newNumbers = Number(numbers.split(","));
+      let sum = 0
+      for (let i=0; i < newNumbers.length; i++) {
+        sum += newNumbers[i];
       }
-    if(count < mode[item]){
-      max = item;
-      count = mode[item];
+      console.log(sum)
+      return sum
     }
+
+        //number that appears most often
+    function modeNumbers(numbers){
+      const newNumber = Number(numbers.split(","));
+      const mode={}
+      let max = 0;
+      let count = 0;
+      for(let i=0; i < newNumber.length; i++){
+        const item = newNumber[i];
+        if(mode[item]) {
+          mode[item]++;
+        }else {
+          mode[item] = 1;
+        }
+      if(count < mode[item]){
+        max = item;
+        count = mode[item];
+      }
+      }
+      console.log(max)
+      return max
     }
-    console.log(max)
-    return max
-  }
 
-
-
-  //add all numbers and divide by number of numbers
-  function averageNumbers(numbers){
-    const newNumbers =Number(numbers.split(","));
-    let divider=newNumbers.length;
-    let sum=0;
-    for(let i=0; i < newNumbers.length; i++) {
-      sum+= newNumbers[i];
+    //add all numbers and divide by number of numbers
+    function averageNumbers(numbers){
+      const newNumbers =Number(numbers.split(","));
+      let divider=newNumbers.length;
+      let sum=0;
+      for(let i=0; i < newNumbers.length; i++) {
+        sum+= newNumbers[i];
+      }
+      console.log(sum/divider)
+      return sum/divider
     }
-    console.log(sum/divider)
-    return sum/divider
-  }
 
+    console.log("line 95")
+}
 
-//if any letters are in the input, return "Invalid input."
-//if no operation is selected, return "Invalid input."
+//className={score>99 ? "visible" : "hidden"}
+//className={error ? "error" : "noError"}
 
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmitEvent}>
+        <input id="values" className="values" type="text" />
+        <select id="operation" className="operation" onChange={handleSelectChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -95,20 +119,19 @@ function Form() {
 
 export default Form;
 
-// note for later:
-// numbers
-//       .split(",")
-//       .reduce((total, current) => (total += Number(current)), 0);
 
-// suggestion from Anima
-// function (numbers) {
-//   let mode = 0;
-//   let occurence = 1;
-//   numbers.split(",").forEach((num) => {
-//     if (numbers.split(num).length > occurence) {
-//       mode = num;
-//       occurence = numbers.split(num).length;
-//     }
-//   });
-//   return mode;
-// }
+/*
+Here are some thoughts:
+I am rewatching the Forms video from 2/7/23.
+You set up the logic and then you need a function to handle the logic.
+Remember that the changing of state is when the user changes something.
+In this case, the user is doing two things: putting in numbers and calculating them. 
+
+The form will be submitted with a button, so we should do something onSubmit.
+The user can choose the operation. Since that is a dropdown menu, that needs to happen onChange. 
+Both of those things should go inside the tag labels.
+
+
+Should the event.prevent.default be in the form function or the handeSubmitEvent function?
+
+*/
