1f59f5f9e4486a8efea6c9301d0641fbf4da33d4
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-14 17:40:15 -0400

    first test passed

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,47 @@
 import React from "react";
 import "./Form.css";
-
+import { useState } from "react";
 function Form() {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    setInput(event.target.value);
+    // if (input.length === 0) {
+    //   setError("Invalid input.");
+    // }
+  };
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    if (input.length === 0) {
+      setError("Invalid input.");
+    }
+    return;
+  };
+  console.log(input);
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
-        <option value=""></option>
-        <option value="sum">sum</option>
-        <option value="average">average</option>
-        <option value="mode">mode</option>
-      </select>
-      <button type="submit">Calculate</button>
-    </form>
+    <div>
+      <form>
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={input}
+          onChange={handleInput}
+        />
+        <select id="operation" name="operation">
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button onClick={handleButtonClick} type="submit">
+          Calculate
+        </button>
+      </form>
+      <div>{error}</div>
+    </div>
   );
 }
 
