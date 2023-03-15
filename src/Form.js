91a9f03c16f45033ae93b846f222309a295e6a77
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-14 20:05:58 -0400

    all index tests are passing

--- src/Form.js
+++ src/Form.js
@@ -4,25 +4,63 @@ import { useState } from "react";
 function Form() {
   const [input, setInput] = useState("");
   const [error, setError] = useState("");
+  const [operation, setOperation] = useState("sum");
+  const [result, setResult] = useState("");
 
   const handleInput = (event) => {
     event.preventDefault();
     setInput(event.target.value);
-    // if (input.length === 0) {
-    //   setError("Invalid input.");
-    // }
+  };
+
+  const handleOperationChange = (event) => {
+    event.preventDefault();
+    setOperation(event.target.value);
+  };
+  const mode = (arr) => {
+    const mode = {};
+    let max = 0,
+      count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+
+      if (mode[item]) {
+        mode[item]++;
+      } else {
+        mode[item] = 1;
+      }
+
+      if (count < mode[item]) {
+        max = item;
+        count = mode[item];
+      }
+    }
+
+    return max;
   };
   const handleButtonClick = (event) => {
     event.preventDefault();
-    if (input.length === 0) {
+    const regex = /^-?\d+(\.\d+)?$/; // regular expression for valid numbers
+    const numbers = input.split(",");
+    const isValidInput = numbers.every((number) => regex.test(number.trim()));
+    const sum = numbers.reduce((acc, curVal) => acc + curVal, 0);
+
+    if (!isValidInput) {
       setError("Invalid input.");
-    }
-    if (typeof input !== "number") {
-      setError("Invalid input.");
+    } else {
+      if (operation === "average") {
+        const average = sum / numbers.length; // calculate average
+        setResult(average.toString()); // update result with the average
+      } else if (operation === "sum") {
+        setResult(sum.toString());
+      } else if (operation === "mode") {
+        setResult(mode(sum));
+      }
     }
     return;
   };
-  console.log(input);
+
+  console.log(input, operation, result);
   return (
     <div>
       <form>
@@ -33,7 +71,11 @@ function Form() {
           value={input}
           onChange={handleInput}
         />
-        <select id="operation" name="operation">
+        <select
+          id="operation"
+          name="operation"
+          onChange={handleOperationChange}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -44,6 +86,7 @@ function Form() {
         </button>
       </form>
       <div>{error}</div>
+      <p>{result}</p>
     </div>
   );
 }
